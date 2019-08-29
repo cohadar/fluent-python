f76dac409e7bd1514e7512eb63ceb4c868eddc90
cohadar <cohadar@gmail.com>
2019-08-29 14:00:47 +0200

    parsing ok

--- lisp/eval.py
+++ lisp/eval.py
@@ -124,6 +124,69 @@ def qu(e):
     return [quote, e]
 
 
+class Tokens():
+    def __init__(self, s):
+        self.tokens = list(tokenize(s))
+        self.tokens.append('(EOL)')
+
+    def head(self):
+        return self.tokens[0]
+
+    def _next(self):
+        self.tokens = self.tokens[1:]
+
+    def _close(self):
+        if self.tokens[0] != ')':
+            ValueError('not found ")"')
+        self._next()
+
+    def parse_expr(self):
+        """
+        >>> Tokens('()').parse_expr()
+        []
+
+        >>> Tokens('(foo)').parse_expr()
+        ['foo']
+
+        >>> Tokens('(foo bar)').parse_expr()
+        ['foo', 'bar']
+
+        >>> Tokens('((foo))').parse_expr()
+        [['foo']]
+
+        >>> Tokens('(cdr (cons (quote a) (quote (b c))))').parse_expr()
+        ['cdr', ['cons', ['quote', 'a'], ['quote', ['b', 'c']]]]
+        """
+        if self.head() == '(':
+            self._next()
+            return self.parse_elist()
+        elif self.head() == ')':
+            raise ValueError('unmatched ")"')
+        elif self.head() == '(EOL)':
+            raise ValueError('premature EOL')
+        else:
+            if len(self.tokens) > 1:
+                raise ValueError('extra stuff after expression')
+            return self.head()
+
+    def parse_elist(self):
+        if self.head() == '(':
+            self._next()
+            ret = [self.parse_elist()]
+            self._close()
+            ret.extend(self.parse_elist())
+            return ret
+        elif self.head() == ')':
+            return []
+        elif self.head() == '(EOL)':
+            raise ValueError('premature EOL')
+        else:
+            ret = [self.head()]
+            self._next()
+            ret.extend(self.parse_elist())
+            return ret
+
+
 def tokenize(s):
     """
     >>> list(tokenize('(cdr (quote (a b c)))'))
