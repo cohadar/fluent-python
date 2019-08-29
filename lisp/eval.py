bfa5d2e67e445e82ce5730e20dde5f6ae92f2ff9
cohadar <cohadar@gmail.com>
2019-08-29 11:26:16 +0200

    cp

--- lisp/eval.py
+++ lisp/eval.py
@@ -1,162 +1,126 @@
 """
 Every LISP data object has 3 properties:
-* identity - memory address of str or Node
-* type - str or Node
-* value - for str self, for Node id
+* identity - memory address of str or list
+* type - str or list
+* value - for str self, for list id
 
 # atoms are symbols
 >>> peval('foo')
 foo
 
 # empty list is also an atom
->>> peval(nil); \
-    _isAtom(nil);
+>>> peval([]); \
+    _isAtom([]);
 ()
 True
 
 # list with one element
->>> prepr(Node('foo'))
+>>> prepr(['foo'])
 (foo)
 
 # list with two elements
->>> prepr(Node('foo', Node('bar')))
+>>> prepr([foo, bar])
 (foo bar)
 
 # nested lists
->>> x = Node(a,       \
-        Node(b,       \
-        Node(Node(c), \
-        Node(d)))); prepr(x)
-(a b (c) d)
+>>> prepr([a, [b, c], d])
+(a (b c) d)
 
 # quote operator
->>> x = li(quote, a); prepr(x); peval(x)
+>>> x = [quote, a]; prepr(x); peval(x)
 (quote a)
 a
 
->>> x = li(quote, li(a, b, c)); prepr(x); peval(x)
+>>> x = [quote, [a, b, c]]; prepr(x); peval(x)
 (quote (a b c))
 (a b c)
 
 # atom operator
->>> x = li(atom, qu(a)); prepr(x); peval(x)
+>>> x = [atom, [quote, a]]; prepr(x); peval(x)
 (atom (quote a))
 t
 
->>> x = li(atom, qu(li(a, b, c))); prepr(x); peval(x)
+>>> x = [atom, qu([a, b, c])]; prepr(x); peval(x)
 (atom (quote (a b c)))
 ()
 
->>> x = li(atom, qu(nil)); prepr(x); peval(x)
+>>> x = [atom, qu([])]; prepr(x); peval(x)
 (atom (quote ()))
 t
 
->>> x = li(atom, li(atom, qu(a))); prepr(x); peval(x)
+>>> x = [atom, [atom, qu(a)]]; prepr(x); peval(x)
 (atom (atom (quote a)))
 t
 
->>> x = li(atom, qu(li(atom, qu(a)))); prepr(x); peval(x)
+>>> x = [atom, qu([atom, qu(a)])]; prepr(x); peval(x)
 (atom (quote (atom (quote a))))
 ()
 
 # eq operator
->>> x = li(eq, qu(a), qu(a)); prepr(x); peval(x)
+>>> x = [eq, qu(a), qu(a)]; prepr(x); peval(x)
 (eq (quote a) (quote a))
 t
 
->>> x = li(eq, qu(a), qu(b)); prepr(x); peval(x)
+>>> x = [eq, qu(a), qu(b)]; prepr(x); peval(x)
 (eq (quote a) (quote b))
 ()
 
->>> x = li(eq, qu(nil), qu(nil)); prepr(x); peval(x)
+>>> x = [eq, qu([]), qu([])]; prepr(x); peval(x)
 (eq (quote ()) (quote ()))
 t
 
 # car operator
->>> x = li(car, qu(li(a, b, c))); prepr(x); peval(x)
+>>> x = [car, qu([a, b, c])]; prepr(x); peval(x)
 (car (quote (a b c)))
 a
 
 # cdr operator
->>> x = li(cdr, qu(li(a, b, c))); prepr(x); peval(x)
+>>> x = [cdr, qu([a, b, c])]; prepr(x); peval(x)
 (cdr (quote (a b c)))
 (b c)
 
 # cons operator
->>> x = li(cons, qu(a), qu(li(b, c))); prepr(x); peval(x)
+>>> x = [cons, qu(a), qu([b, c])]; prepr(x); peval(x)
 (cons (quote a) (quote (b c)))
 (a b c)
 
->>> x = li(cons, qu(a), li(cons, qu(b), li(cons, qu(c), qu(nil)))); prepr(x); peval(x)
+>>> x = [cons, qu(a), [cons, qu(b), [cons, qu(c), qu([])]]]; prepr(x); peval(x)
 (cons (quote a) (cons (quote b) (cons (quote c) (quote ()))))
 (a b c)
 
->>> x = li(car, li(cons, qu(a), qu(li(b, c)))); prepr(x); peval(x)
+>>> x = [car, [cons, qu(a), qu([b, c])]]; prepr(x); peval(x)
 (car (cons (quote a) (quote (b c))))
 a
 
->>> x = li(cdr, li(cons, qu(a), qu(li(b, c)))); prepr(x); peval(x)
+>>> x = [cdr, [cons, qu(a), qu([b, c])]]; prepr(x); peval(x)
 (cdr (cons (quote a) (quote (b c))))
 (b c)
 """
 
 
 def _isAtom(e):
-    return isinstance(e, str)
+    return isinstance(e, str) or e == []
 
 
 def _isNil(e):
-    return str(e) == "()"
-
-
-def _elist(e):
-    if isinstance(e, str):
-        if _isNil(e):
-            return []
-        raise ValueError('atoms have no embedded values: ' + e)
-    else:
-        ret = _elist(_tail(e))
-        ret.insert(0, _head(e))
-        return ret
+    return e == []
 
 
 def _repr(e):
     if isinstance(e, str):
         return e
     else:
-        return "({})".format(" ".join([_repr(v) for v in _elist(e)]))
-
-
-nil = "()"  # enforce singleton?
+        return "({})".format(" ".join([_repr(v) for v in e]))
 
 
 def _head(e):
-    assert isinstance(e, Node)
-    return e._head
+    assert isinstance(e, list)
+    return e[0]
 
 
 def _tail(e):
-    assert isinstance(e, Node)
-    return e._tail
-
-
-class Node():
-    def __init__(self, head, tail=nil):
-        assert head is not None
-        assert tail is not None
-        assert tail == nil or isinstance(tail, Node)
-        self._tail = nil if tail is None else tail
-        self._head = head
-
-
-def li(*elements):
-    """
-    >>> prepr(li(a, b, li(c), d))
-    (a b (c) d)
-    """
-    if not elements:
-        return nil
-    return Node(elements[0], li(*elements[1:]))
+    assert isinstance(e, list)
+    return list(e[1:])
 
 
 def qu(element):
@@ -164,12 +128,14 @@ def qu(element):
     >>> prepr(qu(a))
     (quote a)
     """
-    return Node(quote, Node(element))
+    return [quote, element]
 
 
-def eval(e, a=nil):
-    assert isinstance(e, str) or isinstance(e, Node)
-    assert isinstance(a, str) or isinstance(a, Node)
+def eval(e, a=[]):
+    if e == []:
+        return e
+    assert isinstance(e, str) or isinstance(e, list)
+    assert isinstance(a, str) or isinstance(a, list)
     if isinstance(e, str):
         return e
     if isinstance(_head(e), str):
@@ -180,12 +146,12 @@ def eval(e, a=nil):
         elif str(_head(e)) == 'atom':
             # what is atom has wrong number of args?
             el2 = eval(_head(_tail(e)))
-            return t if _isAtom(el2) else nil
+            return t if _isAtom(el2) else []
         elif str(_head(e)) == 'eq':
             # what if eq has wrong number of args?
             el2 = eval(_head(_tail(e)))
             el3 = eval(_head(_tail(_tail(e))))
-            return t if el2 == el3 else nil
+            return t if el2 == el3 else []
         elif str(_head(e)) == 'car':
             # what if car has wrong number of args?
             el2 = eval(_head(_tail(e)))
@@ -200,7 +166,7 @@ def eval(e, a=nil):
             # what if cons has wrong number of args?
             el2 = eval(_head(_tail(e)))
             el3 = eval(_head(_tail(_tail(e))))
-            return Node(el2, el3)
+            return [el2].append(list(el3))
         else:
             raise ValueError('NYI: {}'.format(_head(e)))
     raise ValueError('NYI')
