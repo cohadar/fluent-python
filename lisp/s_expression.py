daaeb0e8c5af0c9c91b00ce83564aaa82b769b8d
cohadar <cohadar@gmail.com>
2019-08-31 09:30:19 +0200

    meh

---
+++ lisp/s_expression.py
@@ -0,0 +1,9 @@
+klasa koja parsira i unparsira s-expresione
+
+ima inner data koji su list/str ili tuple/str, nije bitno
+
+ima lep quote handling
+
+ima isAtom, isNil i __eq__ __hash__ implementirano, 
+kao i razne potrebne konstruktore i __iter__
+
